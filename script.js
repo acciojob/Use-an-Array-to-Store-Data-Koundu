51fd2a779d32155de004a0881705aca1cbefcb21
Koundu <121346044+Koundu@users.noreply.github.com>
2023-05-19 13:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-
+let yourArray = [
+[
+	{
+		one : 1,
+		two : 2
+	},
+	{
+		three: 1,
+		four: 4
+	}
+],
+	[
+		{
+			a: "a",
+			b: "b"
+		},
+		{
+			c: "c",
+			d: "d"
+		}
+	],
+	[
+		{
+			true: 1,
+			false: 0
+		},
+		{
+			false: 0,
+			true: 1
+		}
+	],
+	[
+		{
+			word1: "Lenovo",
+			word2: "Intel"
+		},
+		{
+			word3: "DELL",
+			word4: "HP"
+		}
+	],
+	[
+		{
+			firstName:"Student_Firstname",
+			lastName:"Student_Lastname"
+		}
+	]
+];
